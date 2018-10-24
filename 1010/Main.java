055b424b4345d3df7cc07102aa0b68274b28ec9a
s18003 <s18003@std.it-college.ac.jp>
2018-10-24 03:36:26 +0000

    10/10の課題

---
+++ 1010/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args){
+		printNumbers(PrimeNumberCalcultor.calcTo(100));
+	}
+
+	private static void printNumbers(List<Integer> numbers) {
+		for (int number: numbers) {
+			System.out.println(number);
+		}
+	}
+}
