cff0a44c40ae86fc65dbe47b7a730be4e467f97e
s18003 <s18003@std.it-college.ac.jp>
2018-10-30 06:43:36 +0000

    10/10修正

--- 1010/Main.java
+++ 1010/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args){
-		printNumbers(PrimeNumberCalcultor.calcTo(100));
+		printNumbers(PrimeNumberCalculator.calcTo(100));
 	}
 
 	private static void printNumbers(List<Integer> numbers) {
--- 1010/PrimeNumberCalculator.java
+++ 1010/PrimeNumberCalculator.java
@@ -1,19 +1,18 @@
 import java.util.*;
 
-public class PrimeNumberCalcultor {
+public class PrimeNumberCalculator {
 	public static List<Integer> calcTo(int maxNumber) {
 		if (maxNumber < 2) {
 			return Collections.emptyList();
 		}
-						
+
 		boolean[] isPrimeNumber = new boolean[maxNumber + 1];
-		for (int testNumber = 3; testNumber <= maxNumber;
-				testNumber = testNumber + 2) {
+		for (int testNumber = 3; testNumber <= maxNumber; testNumber = testNumber + 2) {
 			isPrimeNumber[testNumber] = true;
-				}
+		}
 
 		List<Integer> primeNumbers = new ArrayList<Integer>();
-		PrimeNumbers.add(2);
+		primeNumbers.add(2);
 		for (int testNumber = 3, searchingLimit = (int) Math.sqrt(maxNumber);
 				testNumber <= maxNumber; testNumber = testNumber + 2) {
 			if (!isPrimeNumber[testNumber]) {
@@ -23,8 +22,7 @@ public class PrimeNumberCalcultor {
 			if (testNumber > searchingLimit) {
 				continue;
 			}
-			for (int i = testNumber * 2; i <= maxNumber;
-					i = i + testNumber) {
+			for (int i = testNumber * 2; i <= maxNumber; i = i + testNumber) {
 				isPrimeNumber[i] = false;
 			}
 				}
